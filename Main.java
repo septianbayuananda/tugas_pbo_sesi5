80d766a53eea5497567dafa0d6934d44319f49b1
septianbayuananda <161548263+septianbayuananda@users.noreply.github.com>
2025-03-11 20:10:37 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,14 +11,14 @@ package Projek;
 public class Main {
     public static void main(String[] args) {
         //Membuat Objek Mobil
-        Mobil Avanza = new Mobil (4, "Ranggis", "B 12 CT", "Mobil");
+        Mobil Avanza = new Mobil (4, "Bayu", "B 12 CT", "Mobil");
         System.out.println("=== Informasi Kendaraan ===");
         Avanza.infoKendaraan();
         Avanza.serviceKendaraan();
         Avanza.serviceKendaraan("Ganti OLI");
         
         //Membuat Objek Motor
-        Motor Vario = new Motor (true, "Ranggis", "B 12 CT", "Motor");
+        Motor Vario = new Motor (true, "Bayu", "B 12 CT", "Motor");
         System.out.println("=== Informasi Kendaraan ===");
         Vario.infoKendaraan();
         Vario.infoKendaraan();
